adeed7429cafb890a4b2fc61c9b32f838c5b9fdc
MarinaSusanne <susanne.hagen@live.nl>
2023-04-20 12:16:11 +0200

    eerste start project

---
+++ src/main/java/com/susanne/Susanne_eindopdrachtVA/SusanneEindopdrachtVaApplication.java
@@ -0,0 +1,13 @@
+package com.susanne.Susanne_eindopdrachtVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusanneEindopdrachtVaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusanneEindopdrachtVaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/susanne/Susanne_eindopdrachtVA/SusanneEindopdrachtVaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susanne.Susanne_eindopdrachtVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusanneEindopdrachtVaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
